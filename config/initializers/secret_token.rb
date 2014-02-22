0b708b74a5a08672b4c949dcd3e467875a707abc
Astr0surf3r <astr0surf3r@gmail.com>
2014-02-21 16:12:50 -0800

    primero commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compania::Application.config.secret_token = 'ac13ec2a44900a002a7750db8dd75618191147db0b09300b7a8dbf3a4e570dc76f61982fbd848217e3263ae28cbaababd9bb03b8307fafec6ac02641a3110e06'
